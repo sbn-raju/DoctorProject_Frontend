0026b2b5aa2be7e24f1068b7a1ff0c07c4ba2478
sbn raju <simhardrinagaraju@gmail.com>
2024-07-03 21:51:28 +0530

    Testing Frontend

--- src/pages/Main Pages/AppointmentPage.jsx
+++ src/pages/Main Pages/AppointmentPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import AppointmentImg from '../../assets/Page Assets/Home/appointment-img.png';
 import AppointmentBg from '../../assets/Page Assets/Home/appointment-bg.png';
-import {clockImage} from '../../assets/Page Assets/Home/clock.png';
+import clockImage from '../../assets/Page Assets/Home/clock.png';
 import docter from '../../assets/Page Assets/Home/docter image.png';
 import Input from "../../components/Input Fields/Input";
 import CommonButton from '../../components/Buttons/CommonButton';
